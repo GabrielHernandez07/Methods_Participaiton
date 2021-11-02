3af773440a2e11096c8464cfeb09e7d3a62d73c7
ghern <ghern@DESKTOP-OCUVVH0>
2021-11-02 11:32:49 -0500

    Completed

---
+++ Participation_Simple_Method/Participation_Simple_Method/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Participation_Simple_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter any type of animal.");
+            string animal = Console.ReadLine();
+
+            string noise = Speak(animal);
+            Console.WriteLine($"A {animal} makes the sound of {noise}!");
+            
+        }
+        static string Speak(string animal)
+        {
+            string animalSound = "";
+            if (animal == "cow")
+	        {
+                animalSound = "moo";
+	        }
+            else if (animal == "dog")
+	        {
+                animalSound = "ruff";
+	        }
+            else if (animal == "pig")
+            {
+                animalSound = "oink";
+            }
+            else
+            {
+                animalSound = "AAAAAAHHHHH";
+            }
+            
+            return animalSound;
+        }
+        
+    }
+}
